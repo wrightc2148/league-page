1d22babfe14026f400350dc4565a35edf3610f80
wrightc2148 <144875883+wrightc2148@users.noreply.github.com>
2023-09-13 01:16:31 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "921151976868155392"; // your league ID
 export const leagueName = "NCAA Pac 12"; // your league name
-export const dues = 100; // (optional) used in template constitution page
+export const dues = 0; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
